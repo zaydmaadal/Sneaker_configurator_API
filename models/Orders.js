ca4fbbbfbe254420f86c9721c214c43b4188b685
zaydolas74 <r0934607@student.thomasmore.be>
2024-11-02 22:29:48 +0100

    installing mongoose, cors and bodyParser and adding ordersRouter and controllers and models

---
+++ models/Orders.js
@@ -0,0 +1,29 @@
+//nog geen mongoose
+
+/*const Schema = mongoose.Schema;
+
+const OrderSchema = new Schema({
+  user: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: String,
+    required: true,
+  },
+  status: {
+    type: String,
+    required: true,
+  },
+  amount: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+});
+
+
+module.exports = Order;*/
