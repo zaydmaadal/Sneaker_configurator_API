531c4fcfde003d000607e4d085d8a31f41c265ca
zaydolas74 <r0934607@student.thomasmore.be>
2024-12-05 00:21:12 +0100

    test

--- app.js
+++ app.js
@@ -12,7 +12,9 @@ const ordersRouter = require("./routes/api/v1/orders");
 
 async function connect() {
   try {
-    await mongoose.connect(proces.env.MongoDB_Con);
+    await mongoose.connect(
+      "mongodb+srv://zaydmaadal:zaydolas74@sneaker-configurator.p8sxh.mongodb.net/"
+    );
     console.log("Connected to MongoDB");
   } catch (error) {
     console.error("Could not connect to MongoDB", error);
