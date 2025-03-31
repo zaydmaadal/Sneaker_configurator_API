a7313a62c37fb56c6128975108364f83b9592169
zaydolas74 <r0934607@student.thomasmore.be>
2025-04-01 00:01:33 +0200

    fix

--- app.js
+++ app.js
@@ -1,65 +1,45 @@
-const express = require("express");
-const path = require("path");
-const cookieParser = require("cookie-parser");
-const logger = require("morgan");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const mongoose = require("mongoose");
-const http = require("http");
-const { Server } = require("socket.io");
+const connectDB = require("./config/db");
 
-const indexRouter = require("./routes/index");
-const usersRouter = require("./routes/api/v1/users");
-const ordersRouter = require("./routes/api/v1/orders");
+const usersRoutes = require("./routes/api/v1/users");
+const authRoutes = require("./routes/api/v1/auth");
+const clubsRoutes = require("./routes/api/v1/clubs");
 
-async function connect() {
-  try {
-    await mongoose.connect(
-      "mongodb+srv://zaydmaadal:zaydolas74@sneaker-configurator.p8sxh.mongodb.net/"
-    );
-    console.log("Connected to MongoDB");
-  } catch (error) {
-    console.error("Could not connect to MongoDB", error);
-  }
-}
+require("dotenv").config();
 
-connect().catch(console.error);
+var indexRouter = require("./routes/index");
+var app = express();
 
-const app = express();
-const server = http.createServer(app);
-const io = new Server(server, {
-  cors: {
-    origin: "*", // Of specificeer je frontend-URL
-    methods: ["GET", "POST", "PUT", "DELETE"],
-  },
-});
-
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:3001", // Specifieke frontend-URL
+    credentials: true,
+  })
+);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-app.use("/api/v1/orders", ordersRouter);
 
 app.use("/", indexRouter);
-app.use("/api/v1/users", usersRouter);
 
-// WebSocket-logica
-io.on("connection", (socket) => {
-  console.log("A user connected");
+app.use("/api/v1/users", usersRoutes);
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/clubs", clubsRoutes);
 
-  // Luister naar een aangepast event
-  socket.on("order-updated", (data) => {
-    console.log("Order updated:", data);
-    io.emit("order-updated", data); // Stuur de update naar alle clients
-  });
+connectDB();
 
-  socket.on("disconnect", () => {
-    console.log("A user disconnected");
-  });
-});
-
-//console log link to the API
 console.log("API is running on http://localhost:3000");
+
+//const PORT = process.env.PORT || 3000;
+//app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
 module.exports = app;
